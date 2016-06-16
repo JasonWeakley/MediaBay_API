aadd86ecbb6b1a87bea88c2b30f36f94f8d9e2e3
Jason Weakley <jason.b.weakley@gmail.com>
2016-06-16 14:58:00 -0500

    created context and added dependencies

---
+++ MediaBay/src/MediaBay/Models/MediaBayContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MediaBay.Models
+{
+    public class MediaBayContext : DbContext
+    {
+        public MediaBayContext(DbContextOptions<MediaBayContext> options)
+            : base(options)
+        { }
+
+        public DbSet<AdminUser> AdminUsers { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Group> ProductGroups { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<InvoiceLine> InvoiceLines { get; set; }
+        public DbSet<MediaType> MediaTypes { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Series> ProductSeries { get; set; }
+    }
+}
